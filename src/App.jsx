e88e5c08eb82a9fd153c87d80343f55d8be2292a
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-12-07 00:03:39 +0000

    added to the code.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,31 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [ingredients, setIngredients] = React.useState([]);
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function makeRecipe(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
+  return (
+    <main>
+      <form action={makeRecipe} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+      <ul>{ingredientsListItems}</ul>
+    </main>
+  );
 }
 
 export default App;
