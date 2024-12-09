e2a43fb0c62048fa26475b15338fa28b62ec3b00
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-12-09 17:08:38 +0000

    added comments.

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import "./App.css";
 function App() {
   const [ingredients, setIngredients] = React.useState([]);
 
+  // mapping through each ingredinet.
+  // giving each ingredient a list item along with adding the actual ingredient so it is then returned below in the un ordered list.
   const ingredientsListItems = ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
   ));
 
+  // action allows me to bring in the formData as a paremeter without having to use event.
+  // newIngredient is set to equal the ingredient from the form.
+  // setIngredients then adds to newIngredient onto to prevIngrdients in an array.
   function makeRecipe(formData) {
     const newIngredient = formData.get("ingredient");
     setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
   }
 
+  // form action is set to the makeRecipe function above.
   return (
     <main>
       <form action={makeRecipe} className="add-ingredient-form">
