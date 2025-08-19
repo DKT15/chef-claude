a86c7871a029a33cb2527dcb10780d19d3d56aab
Danté Kellman-Thompson <dante.kellman@icloud.com>
2025-08-19 23:25:50 +0100

    added to the code.

--- src/assets/ai.js
+++ src/assets/ai.js
@@ -4,7 +4,7 @@ const AnthropicAPI = ANTHROPIC_API_KEY;
 
 console.log(AnthropicAPI);
 
-//When interacting with AI, it needs to be given a sysytem prompt.
+//When interacting with AI, it needs to be given a system prompt.
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `;
